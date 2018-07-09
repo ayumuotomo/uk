c5fffd4056a2e1a603d6820e2e0a127cf3612043
ayumu.otomo <aymotm1993@gmail.com>
2018-07-09 06:23:47 +0000

    welcome

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <div class="text-center text-muted">© 2018 THE U.K.</div>
+</footer>
---
+++ resources/views/toppage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>タイトル</title>
+    </head>
+    <body>
+        <p>FOOD</p>
+        <P>LEISURE</P>
+        <p>SPORTS</p>
+        <P>BOOKS</P>
+        <P>MOVIE</P>
+        <P>MUSIC</P>
+        <P>TEXTBOOK</P>
+        <P>OTHERS</P>
+    </body>
+</html>
