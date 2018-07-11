1fc454b64170f7a7baaded7824b27c1dc46092ad
kaho.yoshii <ec2-user@ip-172-31-27-95.us-east-2.compute.internal>
2018-07-11 06:44:17 +0000

    post function

--- resources/views/subcategory/books.blade.php
+++ resources/views/subcategory/books.blade.php
@@ -6,21 +6,29 @@
     <div class="form-group">
         {!! Form::open(['input type' => 'hidden', 'name' => 'subcategory', 'value' => 'books'])!!}
 
-
+    <div class="row">
         <div class="form-group">
+           
+            <div class="col-sm-4">
             <label>Author</label>
             {!! Form::textarea('title', old('title'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '東野圭吾']) !!}
-       </div>
+           </div>
+        </div>
        
-       <div class="form-group">
+        <div class="form-group">
+            <div class="col-sm-4">
            <label>Title</label>
             {!! Form::textarea('subject', old('subject'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '卒業']) !!}
+            </div>
         </div>
         
         <div class="form-group">
+            <div class="col-sm-4">
             <label>Details</label>
             {!! Form::textarea('detail', old('deatail'), ['class' => 'form-control', 'rows' => '2', ]) !!}
+            </div>
         </div>
+    </div>
             {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
 
     {!! Form::close()!!}
--- resources/views/subcategory/others.blade.php
+++ resources/views/subcategory/others.blade.php
@@ -2,28 +2,37 @@
 
 @section('content')
  
+<div class="form-group">
+        {!! Form::open(['input type' => 'hidden', 'name' => 'subcategory', 'value' => 'books'])!!}
 
-    <div class="form-group">
-        {!! Form::open(['input type' => 'hidden', 'name' => 'subcategory', 'value' => 'others'])!!}
-
-
+    <div class="row">
         <div class="form-group">
-            <label></label>
-            {!! Form::textarea('title', old('title'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '']) !!}
-       </div>
+           
+            <div class="col-sm-4">
+            <label>Author</label>
+            {!! Form::textarea('title', old('title'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => ' ']) !!}
+           </div>
+        </div>
        
-       <div class="form-group">
-           <label></label>
-            {!! Form::textarea('subject', old('subject'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '']) !!}
+        <div class="form-group">
+            <div class="col-sm-4">
+           <label>Title</label>
+            {!! Form::textarea('subject', old('subject'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => ' ']) !!}
+            </div>
         </div>
         
         <div class="form-group">
+            <div class="col-sm-4">
             <label>Details</label>
             {!! Form::textarea('detail', old('deatail'), ['class' => 'form-control', 'rows' => '2', ]) !!}
+            </div>
         </div>
+    </div>
             {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
 
     {!! Form::close()!!}
     </div>
+
+  
 
 @endsection
