8d7ac2137cd8450ecbcd1388a9eaa3cb6bad03b1
ayumu.otomo <aymotm1993@gmail.com>
2018-07-11 05:11:00 +0000

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Auth::check())
-        <div class="row">
-            <aside class="col-md-4">
-            </aside>
-            <div class="col-xs-8">
-                    @include('toppage')
-            </div>
-        </div>
-    @else
+   
         <div class="cover">
              <div class="cover-inner">
                 <h1>Your interests lead to new relationships</h1>
                <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">Start The U.K.</a>
+          
                <a href="{{ route('login') }}" class="btn btn-success btn-lg">Login</a>
+               
             </div>
         </div>
-    @endif
+
 @endsection
+
+                        
+                       
