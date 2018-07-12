9303d4ebf4a2ed9f2fd64de11e04b177f78abbe2
ryohei.matsumoto <ec2-user@ip-172-31-23-216.us-east-2.compute.internal>
2018-07-12 04:38:47 +0000

    refresh

--- database/migrations/2018_07_12_133505_create_changing_posts_table.php
+++ database/migrations/2018_07_12_133505_create_changing_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangePostsTable extends Migration
+class CreateChangingPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -36,3 +36,4 @@ class ChangePostsTable extends Migration
         });
     }
 }
+
