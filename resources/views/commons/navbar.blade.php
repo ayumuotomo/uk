452bb2491bf4d0104d70f9efa19ae2b9fb092875
ayumu.otomo <aymotm1993@gmail.com>
2018-07-13 03:32:54 +0000

    login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -5,31 +5,35 @@
 <div class="fullWidth header">
   <div class="container">
     <div class="col-xs-12 col-sm-2 col-md-3 col-md-offset-1 col-lg-4 col-lg-offset-1 header-logo"><a href="/" class="header-logo-name">The U.K.</a></div>
-    <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 header-nav">
-      <ul>
-        <li>
-          <a href="/" class="header-nav-link header-nav-top-link">Home</a>
-        </li>
-        <li>
-          <a href="/" class="header-nav-link header-nav-top-link">Category</a>
-        <ul>
-            <li><a href="subcategory.leisure" class="header-nav-link header-nav-sub-link">LEISURE?</a></li>
-            <li><a href="subcategory.books" class="header-nav-link header-nav-sub-link">BOOKS</a></li>
-            <li><a href="subcategory.music" class="header-nav-link header-nav-sub-link">MUSIC</a></li>
-            <li><a href="subcategory.sports" class="header-nav-link header-nav-sub-link">SPORTS</a></li>
-            <li><a href="subcategory.textbook" class="header-nav-link header-nav-sub-link">TEXTBOOK</a></li>
-            <li><a href="subcategory.others" class="header-nav-link header-nav-sub-link">OTHERS</a></li>
-        </ul>
-         
-         <li>
-          <a href="#" class="header-nav-link header-nav-top-link">{{ Auth::user()->name }}</a>
+      <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 header-nav">
           <ul>
-            <li><a href="{{ route('users.show', Auth::id()) }}" class="header-nav-link header-nav-sub-link">My Page</a></li>
-              <li><a href="{{ route('logout.get') }}" class="header-nav-link header-nav-sub-link">Logout</a></li>
+            <li>
+                <a href="/" class="header-nav-link header-nav-top-link">Home</a>
+            </li>
+            <li>
+                <a href="/" class="header-nav-link header-nav-top-link">Category</a>
+            <ul>
+            
+                   @foreach(config('app.category') as $category)
+            <li><a href="subcategory/{{ ($category['name']) }}"  class="header-nav-link header-nav-sub-link">{{strtoupper($category['name'])}}</a></li>
+              @endforeach
+            </ul>
+      
+             <li>
+                <a href="#" class="header-nav-link header-nav-top-link">
+                  @auth
+                  {{ Auth::user()->name }}
+                  @else
+                  
+                  @endauth
+                </a>
+              <ul>
+                <li><a href="{{ route('users.show', Auth::id()) }}" class="header-nav-link header-nav-sub-link">My Page</a></li>
+                <li><a href="{{ route('logout.get') }}" class="header-nav-link header-nav-sub-link">Logout</a></li>
             
        
-          </ul>
-        </li>                  
+              </ul>
+            </li>                  
            
                    
     </div>
