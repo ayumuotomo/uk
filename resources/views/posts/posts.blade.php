d23373fc7be82affaf02f817d911200f1e1387c9
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-07-18 04:40:08 +0000

    under button

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,48 +4,48 @@
 @foreach ($posts as $post)
     <?php $user = $post->user; ?>
     <li class="media">
+        
+    <div class="container">
         <div class="media-body">
            
-
-           <div class="col-md-6">
-           <div class="well">
-          <div style="display:inline-flex">
-            <div class="text-left">
-                <h3>{!! nl2br(e($post->title)) !!}</h3> 
-            </div>
-            
-            &nbsp;&nbsp;
-            
-               <div class="text-center">
-                <h3>{!! nl2br(e($post->subject)) !!}</h3>　
-               </div>
-            </div>
-            </div>
-            </div>
-            
-                 
-                 <div>
-               <span class="text-muted">posted at {{ $post->created_at }}</span>
-                </div>
-            
+           <div class="row">
+               <div class="col-md-6">
+                   <div class="well">
+                       <div style="display:inline-flex">
+                            <div class="text-left">
+                                <h3>{!! nl2br(e($post->title)) !!}</h3> 
+                            </div>
+                            &nbsp;&nbsp;
+                           <div class="text-center">
+                                <h3>{!! nl2br(e($post->subject)) !!}</h3>　
+                            </div>
+                       </div>
+                    </div>
+            <span class="text-muted">posted at {{ $post->created_at }}</span>
            <div style="display:inline-flex">
                @include('post_favorite.favorite_button', ['user' => $user])     
                 
            
                 @if (Auth::user()->id == $post->user_id)
                     {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn']) !!}
                     {!! Form::close() !!}
                 @endif
                 
-                
                    <a href="{{ url('posts/'.$post->id.'/show') }}">detail</a>
-          </div>
-             
+            </div>
+
+                </div>
+            <div class="col-md-6"></div>
+            </div>
+            
+          
 
 
-        </div>
+</div>
+</div>
     </li>
+    
     
 @endforeach
 
