736898d4b0a80a7bd40ac3f42d042543be68bb85
kaho.yoshii <ec2-user@ip-172-31-27-95.us-east-2.compute.internal>
2018-07-23 01:26:47 +0000

    brushup2

--- resources/views/posts/detail.blade.php
+++ resources/views/posts/detail.blade.php
@@ -5,64 +5,46 @@
 
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-    <div class="col-lg-12 text-center">
-    <div class="panel panel-default">
-        
-    <ul class="media-list">
 
+   
+<div class="col-lg-12 text-center">
+   
     <?php $user = $post->user; ?>
-    <li class="media">
-        <div class="media-body">
+        <!--li class="media">
+        <div class="media-body"-->
             
             <br>
             <br>
             <br>
             
-        <ul class="offset-3 col-md-6" "list-group">
+    <ul class="offset-3 col-md-6" "list-group">
             
-               <div class="text-muted">TITLE</div>
-               <li class="list-group-item"> {!! nl2br(e($post->title)) !!}</li>  
-    <br>           
-               <div class="text-muted">CONTENTS</div>
-            　 <li class="list-group-item"> {!! nl2br(e($post->subject))!!}</li>
-    <br>
-               <div class="text-muted">DETAILS</div>
-            　 <li class="list-group-item">{!! nl2br(e($post->detail)) !!}</li> <br>
+        <div class="text-muted">TITLE</div>
+        <li class="list-group-item"> {!! nl2br(e($post->title)) !!}</li>  
+            <br>           
+        <div class="text-muted">CONTENTS</div>
+        <li class="list-group-item"> {!! nl2br(e($post->subject))!!}</li>
+            <br>
+        <div class="text-muted">DETAILS</div>
+        <li class="list-group-item">{!! nl2br(e($post->detail)) !!}</li> 
+            <br>
       
-        </ul>
+    </ul>
         
-    <br>
-    <br>
+            <br>
+            <br>
     
-        <ul class="timeline">       
-          <li class="timeline-inverted">
-                <div class="timeline-image">
-                  <h4>
-            BY {!! nl2br($user->name) !!}</h4>
-            </div>
-            </li>
-            </ul>
+        
+        <h4>BY {!! nl2br($user->name) !!}</h4>
             
-             <div class="pull-center">  
+        <div class="pull-center">
             @include('post_favorite.favorite_button', ['user' => $user])     
-            </div>
-            </div>
-     
-               <div class="text-right">
-               <span class="text-muted">posted at {{ $post->created_at }}</span>
-               </div>
-               
-               <br>
-               <br>
-            
-           
-           
-           
-             
         </div>
-    </li>
+                
+</div>
+     
+<div class="text-right">
+    <span class="text-muted">posted at {{ $post->created_at }}</span>
+</div>
 
-</ul>
 @endsection
