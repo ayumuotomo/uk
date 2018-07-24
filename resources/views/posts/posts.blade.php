3acd0f7e37efaf1a702c3ac88d41bc950cedf046
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-07-24 00:53:26 +0000

    postbladefixed

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,129 +1,28 @@
-@extends('layouts.app')
-
-@section('content')
-<ul class="media-list">
+<div class="kaho">
+<div class="row post-list">
 @foreach ($posts as $post)
-    <?php $user = $post->user; ?>
-    <li class="media">
-    <div class="container">
-        <div class="media-body">
-           <div class="col-sm-2">
-            <div class="row">
-            <div class="col-lg-12">
+ <?php $user = $post->user; ?>
+    <div class="col-sm-4">
+        <div class="comment">
+            <a href="{{ url('posts/'.$post->id.'/show') }}" >
+                <h3>{!! nl2br(e($post->title)) !!}<br /> 
+                {!! nl2br(e($post->subject)) !!}</h3>　
 
-
-            <ul class="timeline">
-                <li class="timeline-inverted">
-                <div class="timeline-image">
-                        <a href="{{ url('posts/'.$post->id.'/show') }}" >
-                            <!--<div style="display:inline-flex\">-->
-                               <div class="panel-body">
-                                   <h3>{!! nl2br(e($post->title)) !!}<br /> 
-                                    {!! nl2br(e($post->subject)) !!}</h3>　
-                                </div>
-                        </a>
-                </div>
-                </li>
-            </ul>
-
-
-
-            <span class="text-muted">posted at {{ $post->created_at }}</span>
-            
-            <div　class >
-              
-                @if (Auth::user()->id == $post->user_id)
-                    {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn']) !!}
-                    {!! Form::close() !!}
-                @endif
-                
-
+            </a>
             </div>
-            
-</div>
-</div>
-</div>
-</a>
-</div>
-    </li>
-    @endforeach
-</ul>
-<<<<<<< HEAD
-<!--@section('content')
-<ul class="media-list">
-@foreach ($posts as $post)
-    <?php $user = $post->user; ?>
-    <li class="media">
-    <div class="container">
-        <div class="media-body">
-           <div class="col-sm-2">
-            <div class="row">
-            <div class="col-lg-12">
-
-<li class="media">
-    <div class="container">
-        <div class="media-body">
            
-           <div class="col-md-12">
-               <table class="table table-bordered">
-                             <a href="{{ url('posts/'.$post->id.'/show') }}" >
-          
-            <div class="row">
-                         <div class="panel panel-default">　
-                            <div style="display:inline-flex">
-                               
-                               <div class="panel-body">
-                                   <div class="pull-left">
-                                   <h3>{!! nl2br(e($post->title)) !!}</h3> 
-                               </div>
-                               </div>
-                            &nbsp;&nbsp;
-
-            <ul class="timeline">
-                <li class="timeline-inverted">
-                <div class="timeline-image">
-                        <a href="{{ url('posts/'.$post->id.'/show') }}" >
-                            <!--<div style="display:inline-flex\">
-                               <div class="panel-body">
-                                   <h3>{!! nl2br(e($post->title)) !!}<br /> 
-                                    {!! nl2br(e($post->subject)) !!}</h3>　
-                                </div>
-<<<<<<< HEAD
-                        </a>
-                </div>
-                </li>
-            </ul>
-
-
-
-
-            <span class="text-muted">posted at {{ $post->created_at }}</span>
+ <span class="text-muted">{{ $post->created_at }}</span>
             
-            <div　class >
-              
                 @if (Auth::user()->id == $post->user_id)
                     {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn']) !!}
                     {!! Form::close() !!}
                 @endif
-                
-
-            </div>
-            
-</div>
-</div>
-               
-            
-          </div>
-           </a>
-</div>
-    </li>
-    @endforeach
-</ul>    
-
+    </div>
     
+@endforeach
+</div>
 
 
--->
-
+<div class="clearfix"></div>
+</div>
