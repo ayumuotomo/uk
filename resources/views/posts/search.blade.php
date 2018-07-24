ab13fc735208b9389458a00670ae536f3f6ba1e7
ryohei.matsumoto <ec2-user@ip-172-31-23-216.us-east-2.compute.internal>
2018-07-24 05:23:37 +0000

    finish

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,22 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container-fluid">
-<div class="row">
-<!--↓↓ 検索フォーム ↓↓-->
-<div class="col-sm-4" style="padding:20px 0; padding-left:0px;">
-<form class="form-inline" action="/posts/search">
-  <div class="form-group">
-   
-        <input type="text" name="keyword" value="{{$keyword}}" class="form-control" placeholder="キーワードを入力してください！！">
-    
-  </div>
-  <input type="submit" value="検索" class="btn btn-info">
-</form>
-</div>
-
-<!--↑↑ 検索フォーム ↑↑-->
- 
+<br><br><br><br><br><br>
 <div class="col-sm-8" style="text-align:right;">
   <div class="paginate">
   {{-- $Bdata->appends(Request::only(keyword))->links() --}}
