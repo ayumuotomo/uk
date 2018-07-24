a84a3799408a71463bae0989978b36309ad79fdb
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-07-24 05:33:37 +0000

    toppageetc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,20 @@
           <div class="intro-lead-in">Your interests lead to new relationships!</div>
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="{{ route('signup.get') }}">Sign Up</a>
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="{{ route('login') }}">Log In</a>
+   
+   <div class="clearfix"></div>
+   
+            <div class="container" id="fukidashi">
+              <img src="img/icon.jpg">
+                <span class="arrow">For more information...</span>
+                  <p class="box23">
+                    同期の好きなことを発見しよう  
+                  </p>
+            </div>
+   
         </div>
       </div>
     </header>
-        
-        
 
 @endsection
 
