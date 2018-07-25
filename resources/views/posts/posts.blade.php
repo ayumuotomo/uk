661af914e0a809e81f4b3667eebd55ffb561bc3d
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-07-25 06:45:14 +0000

    update

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -14,12 +14,7 @@
            
  <span class="text-muted">{{ $post->created_at }}</span>
             
-            
-               @if (Auth::user()->id == $post->user_id)
-                    {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn']) !!}
-                    {!! Form::close() !!}
-                @endif
+               
     </div>
 
     
